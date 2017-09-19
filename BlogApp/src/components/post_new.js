50bc57dc062ea1a7189785678db64517a2d21b1e
mpacek <m.pacek@onet.eu>
2017-09-19 15:56:54 +0200

    BlogAdd validation has been exchanged

--- BlogApp/src/components/post_new.js
+++ BlogApp/src/components/post_new.js
@@ -3,22 +3,33 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -34,6 +45,7 @@ class PostNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">submit</button>
       </form>
     );
   }
