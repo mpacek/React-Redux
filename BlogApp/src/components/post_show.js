838c37b77f9406b8d9be0c1b18e7f641b92937ec
mpacek <m.pacek@onet.eu>
2017-09-25 14:04:43 +0200

    BlogApp added show post component

--- BlogApp/src/components/post_show.js
+++ BlogApp/src/components/post_show.js
@@ -1,13 +1,34 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    console.log(post);
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Spost Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
-    )
+    );
   }
 }
 
-export default PostShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostShow);
