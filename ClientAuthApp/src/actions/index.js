6987f20f1e049395bfa6fcbf53ff5e53909de7b4
mpacek <m.pacek@onet.eu>
2017-10-11 13:22:16 +0200

    ClientAuthApp signout

--- ClientAuthApp/src/actions/index.js
+++ ClientAuthApp/src/actions/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import {
+  AUTH_USER,
+  UNAUTH_USER,
+  AUTH_ERROR
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -20,7 +24,22 @@ export function signinUser({ email, password }) {
       .catch(() => {
         // If request is bad...
         // - Show an error to the user
-
+        dispatch(authError('Bad Login Info'));
       });
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+  
+  return {
+    type: UNAUTH_USER
   }
 }
