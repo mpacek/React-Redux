8e76f2e38b1185446ea7c7d3bbc82452bc934c8e
mpacek <m.pacek@onet.eu>
2017-10-11 16:29:21 +0200

    ClientAuthApp removed unused IndexRoute import

--- ClientAuthApp/src/index.js
+++ ClientAuthApp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, IdexRoute, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
