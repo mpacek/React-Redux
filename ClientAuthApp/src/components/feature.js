d624fec79b4110b0ab2df04e17f156110c7e1cc7
mpacek <m.pacek@onet.eu>
2017-10-12 13:28:06 +0200

    ClientAuthApp added feature component, and HOC require auth

---
+++ ClientAuthApp/src/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends React.Component {
+  render() {
+    return (
+      <div>This is a feature</div>
+    );
+  }
+}
+
+export default Feature;
