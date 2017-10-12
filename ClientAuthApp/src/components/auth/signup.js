294e67971e1d0beca62b61ec341fc77530f50713
mpacek <m.pacek@onet.eu>
2017-10-12 10:31:20 +0200

    ClientAuthForm added more validation

--- ClientAuthApp/src/components/auth/signup.js
+++ ClientAuthApp/src/components/auth/signup.js
@@ -7,10 +7,12 @@ class Signup extends Component {
     const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
 
     return (
+      // There an example how to optimize that repeatable elements in other project
       <form>
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
@@ -20,6 +22,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input className="form-control" {...passwordConfirm} type="password" />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign Up</button>
       </form>
@@ -29,6 +32,18 @@ class Signup extends Component {
 
 function validate(formProps) {
   const errors = {};
+
+  if (!formProps.email) {
+    errors.email = "Please enter an email";
+  }
+
+  if (!formProps.password) {
+    errors.password = "Please enter a password";
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "Please enter a password confirmation";
+  }
 
   if (formProps.password !== formProps.passwordConfirm) {
     errors.password = 'Password must match';
