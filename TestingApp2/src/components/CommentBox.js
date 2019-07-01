ca495901ef7ff31b52174ead42d6c06c1c08e039
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2019-07-01 16:36:42 +0200

    TestinApp2 Redux setup WIP

--- TestingApp2/src/components/CommentBox.js
+++ TestingApp2/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = {
@@ -11,6 +13,9 @@ class CommentBox extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
+
     this.setState({ comment: '' });
   };
 
@@ -27,4 +32,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);
