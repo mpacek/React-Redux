a6d4f014b140e16d70472eb10468d3d5dce9ce78
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2019-07-01 13:58:08 +0200

    TestinApp2 App, and CommentBox tests

--- TestingApp2/src/components/__tests__/App.test.js
+++ TestingApp2/src/components/__tests__/App.test.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
-it('shows a comment box', () => {});
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
